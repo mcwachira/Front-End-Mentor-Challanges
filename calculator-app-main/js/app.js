0f4e8984da2ae4bb156bb5ec824d036c58d6841e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-06-25 13:21:53 +0300

    updated the app to show a value when an button is clicked and to clear and delete the values on the screen

--- calculator-app-main/js/app.js
+++ calculator-app-main/js/app.js
@@ -1,8 +1,9 @@
-const { TripOriginSharp } = require("@mui/icons-material");
 
-const NumbersButtons =document.querySelectorAll('.numbers');
+
+const NumberButtons =document.querySelectorAll('.number');
 const OperatorButton = document.querySelectorAll('.operator');
 const ResetButton = document.querySelector('.clear')
+const DeleteButton = document.querySelector('.delete')
 const EqualsButton = document.querySelector('.equals')
 const PreviousValue = document.querySelector('.previous')
 const CurrentValue = document.querySelector('.current')
@@ -11,18 +12,64 @@ class Calculator{
     constructor(PreviousValue, CurrentValue){
         this.PreviousValue = PreviousValue;
         this.CurrentValue = CurrentValue;
+        this.clear()
     }
-  
-     
+  clear(){
+    this.previousOperand=""
+    this.currentOperand =""
+ 
+  }
+    delete() {
+        this.currentOperand = CurrentValue.innerText.slice(0, CurrentValue.innerText.length)
+    
+        console.log(this.currentOperand)
+    }
+
+
     appendNumber(number){
         if(number === "." && this.currentOperand.includes(".")) return
         this.currentOperand = this.currentOperand.toString() + number.toString();
+      
     }
+    getDisplayNumber(number) {
+        const floatNumber = parseFloat(number);
+        if(isNaN(floatNumber)) return ""
+        return floatNumber.toLocaleString('en')
+    }
+
+
+
+    updateDisplay(){
+      
+        this.CurrentValue.innerText = this.getDisplayNumber(this.currentOperand);
+        console.log(this.CurrentValue.innerText)
+    }
+
+
 }
 
 const calculator = new Calculator(PreviousValue, CurrentValue)
-    NumbersButtons.map((button) => {
+   
+//displaying value on the calculator screen
+NumberButtons.forEach((button) => {
             button.addEventListener('click', () => {
-                calculator.append(button.innerText)
+                calculator.appendNumber(button.innerText)
+                calculator.updateDisplay()
             })
+    })
+
+    console.log(NumberButtons)
+
+
+    //deletes all the values on the screen;
+    ResetButton.addEventListener('click',() => {
+        calculator.clear();
+        calculator.updateDisplay();
+    })
+
+
+    DeleteButton.addEventListener('click', () => {
+        calculator.delete();
+        calculator.updateDisplay();
+
     })
